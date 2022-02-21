d9c32f7cc3baacec715d0a0df5973f5d48119182
AC89 <82317889+AC899@users.noreply.github.com>
2022-02-21 17:16:15 +0000

    Genesis initial state

--- main.py
+++ main.py
@@ -1,28 +1,15 @@
 # This is a sample Python script.
 
 import random
-import array
-
-# maximum number of characters for password generator
-lengthofpassword = 12
-if lengthofpassword >12:
-    print("Maximum number of characters is 12")
-
-#Lists of all possible combinations
-
-digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-
-up_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-                     'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q',
-                     'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
-                     'z']
-
-hi_letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
-                     'I', 'J', 'K', 'M', 'N', 'O', 'P', 'Q',
-                     'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
-                     'Z']
-
-symbols = ['@', '#', '$', '%', '=', ':', '?', '.', '/', '|', '~', '>',
-           '*', '(', ')', '<']
+import string
 
 
+def passwordgen(input):
+    n = int(input("Enter chars"))
+    if n >=16:
+        return("The maximum character limit for passwords is 16 - please try again")
+    else:
+        return(''.join(random.choice(string.ascii_uppercase + string.digits + string.ascii_lowercase) for _ in range(n)))
+
+
+print(passwordgen(input))
