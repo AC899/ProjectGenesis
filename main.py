ceabe9bd63b47ab893ae22f1e039a0a3178143eb
AC89 <82317889+AC899@users.noreply.github.com>
2022-02-21 13:31:24 +0000

    Genesis initial state

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 # This is a sample Python script.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import random
+import array
 
-x= [4,5,6]
+# maximum number of characters for password generator
+lengthofpassword = 12
+if lengthofpassword >12:
+    print("Maximum number of characters is 12")
 
-for i in x:
-    print("Leave my house on the count of: " + str(i))
+#Lists of all possible combinations
+
+digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+up_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+                     'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q',
+                     'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+                     'z']
+
+hi_letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+                     'I', 'J', 'K', 'M', 'N', 'O', 'P', 'Q',
+                     'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+                     'Z']
+
+symbols = ['@', '#', '$', '%', '=', ':', '?', '.', '/', '|', '~', '>',
+           '*', '(', ')', '<']
+
 
