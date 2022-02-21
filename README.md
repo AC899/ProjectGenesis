bd2e4c56cd174f56a67b4045dbe5a0ed5872b113
AC89 <82317889+AC899@users.noreply.github.com>
2022-02-21 12:09:11 +0000

    Genesis initial state

---
+++ README.md
@@ -0,0 +1,1 @@
+#Check the requirements .txt to see the dependencies needed
---
+++ main.py
@@ -0,0 +1,10 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+x= [4,5,6]
+
+for i in x:
+    print("Leave my house on the count of: " + str(i))
+
